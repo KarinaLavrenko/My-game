95f004597c2165c3a273b89fbacc0bf4036ff873
KarinaLavrenko <158204760+KarinaLavrenko@users.noreply.github.com>
2024-02-05 18:30:31 +0200

    Змінили колір фону

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ class Main extends Phaser.Scene {
 
     // This function essentially loads things into our game
     preload() {
-        this.load.spritesheet('plane', 'assets/planesheet.png', { frameWidth: 98, frameHeight: 83 });
+        this.load.spritesheet('girl', 'assets/girl.png', { frameWidth: 88, frameHeight: 60 });
         this.load.image('pipe', 'assets/pipe.png');
         this.load.audio('jump', 'assets/jump.wav');
     }
@@ -12,30 +12,28 @@ class Main extends Phaser.Scene {
     //  create objects within our game such as animations, collision detectors, text, groups, and much more
     create() {
         //Додаємо літак на сцену
-        this.plane = this.physics.add.sprite(0, 0, 'plane')
+        this.girl = this.physics.add.sprite(0, 0, 'girl')
         //Масштабуємо літак
-        this.plane.setScale(0.65, 0.65);
+        this.girl.setScale(0.65, 0.65);
         //Встановлюємо опорну точку літака
-        this.plane.setOrigin(0, 0.5);
+        this.girl.setOrigin(0, 0.5);
 
         //Створимо анімацію літака та налаштуємо для нього гравітацію
         this.anims.create({
-            key: "planeAnimation",
-            frames: this.anims.generateFrameNumbers('plane', { frames: [0, 1, 3, 2] }),
+            key: "girlAnimation",
+            frames: this.anims.generateFrameNumbers('girl', { frames: [0, 1] }),
             frameRate: 10,
             repeat: -1
         });
-        this.plane.play("planeAnimation");
+        this.girl.play("girlAnimation");
 
-        this.plane.body.gravity.y = 400;
+        this.girl.body.gravity.y = 400;
         //створимо об’єкт клавіші “Пробіл”
         this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
         //Створимо текстовий напис для рахунку
         this.score = 0;
         this.labelScore = this.add.text(20, 20, "0", { fontSize: 40, color: "black" });
-        this.avtor = this.add.text(2, 2, "Created by Karina ", { fontSize: 14, color: "pink" });
-
+        this.avtor = this.add.text(2, 2, "Created by Karina ", { fontSize: 14, color: "black" });
         //Створимо труби. Ідея полягає в тому, що кожна труба буде створюватись циклічно через кожні 1.5 секунди. Додаємо групу фізичних об’єктів та обробляємо циклічну подію:
         this.pipes = this.physics.add.group();
 
@@ -47,18 +45,18 @@ class Main extends Phaser.Scene {
         });
 
         //
-        this.physics.add.overlap(this.plane, this.pipes, this.hitPipe, null, this);
+        this.physics.add.overlap(this.girl, this.pipes, this.hitPipe, null, this);
 
     }
 
     // While preload() and create() run only once at the start of the game, update() runs constantly.
     update() {
         //зробимо нахил літака на кут 20, а також перезапустимо гру, якщо літак вилетить за межі сцени
-        if (this.plane.angle < 20) {
-            this.plane.angle += 1;
+        if (this.girl.angle < 20) {
+            this.girl.angle += 1;
         }
 
-        if (this.plane.y < 0 || this.plane.y > 590) {
+        if (this.girl.y < 0 || this.girl.y > 490) {
             this.scene.restart();
         }
         //
@@ -71,12 +69,12 @@ class Main extends Phaser.Scene {
     //
     jump() {
         this.tweens.add({
-            targets: this.plane,
+            targets: this.girl,
             angle: -20,
             duration: 100,
             repeat: 1
         });
-        this.plane.body.velocity.y = -150;
+        this.girl.body.velocity.y = -150;
     }
 
     //Функція для створення блоку труби
@@ -102,10 +100,10 @@ class Main extends Phaser.Scene {
 
     //метод-обробник зіткнення:
     hitPipe () {
-        if (this.plane.alive == false) return;
+        if (this.girl.alive == false) return;
     
         this.timedEvent.remove(false);
-        this.plane.alive = false;
+        this.girl.alive = false;
     
         this.pipes.children.each(function(pipe) {
             pipe.body.velocity.x = 0;
@@ -115,10 +113,10 @@ class Main extends Phaser.Scene {
 
 const config = {
     type: Phaser.AUTO,
-    width: 500,
+    width: 800,
     height: 600,
     scene: Main, // Цю сцену ми створимо на 4-му кроці
-    backgroundColor: '#71c5cf',
+    backgroundColor: 'cf71c5',
     physics: {
         default: 'arcade',
         arcade: {
